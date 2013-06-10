026d2f73fcf9a2f0686bdd21aa2335be93aa19d1
Maxim Küpper <maxim.kuepper@gmail.com>
2013-06-10 20:45:46 +0200

    adding basic response

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,24 +1,47 @@
-class ResourcesController < ApplicationController
+ class ResourcesController < ApplicationController
   before_filter :load_resource
   before_filter :load_kart, only: :checkin
+  skip_before_filter :verify_authenticity_token, only: :checkin
 
   def checkin
-    # Save checkin
-    @kard.resources << @resource
+
+    unless @resource.nil? or @kard.nil?
+      # Save checkin
+      @kard.resources << @resource
+
+      send_response(checkins: @kard.resources.where(id: @resource.id).count)
+    else
+      errors = {}
+
+      errors[:resource] = "Resource missing." if @resource.nil? and params[:resource].nil?
+      errors[:resource] = "Resource not found." if @resource.nil? and params[:resource].present?
+      errors[:kard_uid] = "YAK-UID missing."  if @kard.nil?
+
+      send_response({errors: errors})
+    end
   end
 
   protected
 
   def load_resource
     @resource = Resource.find_by_key params[:resource]
-
-    Rails.logger.debug @resource.inspect
-    render :file => 'public/404', format: [:html], :status => :not_found, :layout => false if @resource.nil?
   end
 
   def load_kart
-    @kard = YetAnotherKard.find_or_create_by_uid params[:kard_uid]
+    @kard = YetAnotherKard.find_or_create_by_uid params[:yak_uid] unless params[:yak_uid].nil?
+
     Rails.logger.debug @kard.inspect
-    render :file => 'public/404', format: [:html], :status => :not_found, :layout => false if @kard.nil?
+  end
+
+  def send_response(response)
+    respond_to do |format|
+      format.json do
+        render :json => {success: (response[:errors].blank? ? true : false)}.merge(response).to_json
+      end
+      format.xml do
+        render :xml => {success: (response[:errors].blank? ? true : false)}.merge(response).to_xml
+      end
+      format.html {render :file => 'public/404.html', :status => :not_found, :layout => false}
+    end
   end
 end
